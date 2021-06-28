10f4b491b4a0f859720ea5241d1411a4d5c0ed07
Raghav-cpp <84151102+Raghav-cpp@users.noreply.github.com>
2021-06-28 19:00:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        I am RAGHAV VERMA.This is an Amazing Bootcamp.I learned a lot in it
+        which will help me in the future.THANK YOU{" "}
+      </p>
+    </div>
+  );
+}
+export default Note;
